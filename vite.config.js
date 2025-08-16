9ab0749fa27402462a461b91941a9e5107619abb
Ajit <ajitkr5501@gmail.com>
2025-08-17 01:55:35 +0530

    cc

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'dist' // output folder for Vercel
-  }
+  base: './', // use relative paths for scripts and CSS
 })
