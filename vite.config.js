3358f1f3b0b597877cb8c5352b1293664c4b9295
Ajit <ajitkr5501@gmail.com>
2025-08-17 01:34:46 +0530

    Again Add vercel.json to fix SPA refresh

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './', // use relative paths for scripts and CSS
+  build: {
+    outDir: 'dist'  // output folder for Vercel
+  }
 })
